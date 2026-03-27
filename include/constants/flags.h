dc0fd1265c12465c9302676ee1bd1644987f7d7b
Robert Houeland <angryrobert123@gmail.com>
2026-03-27 11:18:20 -0700

    add scientist outside rustboro gym
    Add event where Dr Parker talks to player after Rustboro Gym

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -460,7 +460,7 @@
 #define FLAG_REGISTERED_DRAKE                (TRAINER_REGISTERED_FLAGS_START + REMATCH_DRAKE)
 #define FLAG_REGISTERED_WALLACE              (TRAINER_REGISTERED_FLAGS_START + REMATCH_WALLACE)
 
-#define FLAG_UNUSED_0x1AA                    0x1AA // Unused Flag
+#define FLAG_HIDE_RUSTBORO_CITY_DR_PARKER    0x1AA // Was unused
 #define FLAG_UNUSED_0x1AB                    0x1AB // Unused Flag
 
 #define FLAG_DEFEATED_DEOXYS                 0x1AC
